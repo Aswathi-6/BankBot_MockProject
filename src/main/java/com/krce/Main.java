0329868224de4b8b4e4f64ae7624f67e5c882a02
Aswathi-6 <aswathimurugesan2406@gmail.com>
2026-05-11 11:37:54 +0530

    Initial Commit

---
+++ src/main/java/com/krce/Main.java
@@ -0,0 +1,16 @@
+package com.krce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        WebDriver driver=new ChromeDriver();
+//        driver.get("https://demo.guru99.com/V4/index.php");
+//        driver.findElement(B)
+
+        }
+    }
+
