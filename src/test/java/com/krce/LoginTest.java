d160d689bebed83f76ae706d30225e0c39259b28
Aswathi-6 <aswathimurugesan2406@gmail.com>
2026-05-11 11:44:09 +0530

    Added LoginTest java file

---
+++ src/test/java/com/krce/LoginTest.java
@@ -0,0 +1,41 @@
+package com.krce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class LoginTest {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demo.guru99.com/V4/index.php");
+    }
+
+    @Test(dataProvider = "loginData")
+    public void verifyLogin(String username,String password) {
+        LoginPage login=new LoginPage(driver);
+        login.login(username, password);
+        String title=driver.getTitle();
+        Assert.assertTrue(title.contains("Guru99"));
+    }
+
+    @DataProvider(name = "loginData")
+    public Object[][] getData() {
+
+        return new Object[][]{
+                {"mngr661030","pysabyj"}
+        };
+    }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
